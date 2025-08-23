2fea15afbfd30609271449db0639ac0e3d94d847
Mickel Ishema Murenzi <murenzimickel1@gmail.com>
2025-08-23 13:43:15 -0400

    Fix Issue 1:
    1) Move text generation functionality to view package for better separation of concerns

--- code/Lab 1/src/edu/westga/cs1302/lab1/model/Bill.java
+++ code/Lab 1/src/edu/westga/cs1302/lab1/model/Bill.java
@@ -38,30 +38,15 @@ public class Bill {
 		this.items.add(item);
 	}
 	
-	/** Return a String containing the list of bill items and total for the bill.
+	/**Gets the items listed on the bill
 	 * 
 	 * @precondition none
 	 * @postcondition none
 	 * 
-	 * @return a String containing the list of bill items and total for the bill
+	 * @return list of items in the bill
 	 */
-	public String getText() {
-		String text = "ITEMS" + System.lineSeparator();
-		double subTotal = 0.0;
-		for (BillItem item : this.items) {
-			text += item.getName() + " - " + item.getAmount() + System.lineSeparator();
-			subTotal += item.getAmount();
-		}
-		
-		text += System.lineSeparator();
-		text += "SUBTOTAL - $" + subTotal + System.lineSeparator();
-		double tax = subTotal * TAX_RATE;
-		double tip = subTotal * TIP_RATE;
-		text += "TAX - $" + tax + System.lineSeparator();
-		text += "TIP - $" + tip + System.lineSeparator();
-		text += "TOTAL - $" + (subTotal + tip + tax);
-		
-		return text;
+	public ArrayList<BillItem> getItems() {
+		return this.items;
 	}
 
 }
