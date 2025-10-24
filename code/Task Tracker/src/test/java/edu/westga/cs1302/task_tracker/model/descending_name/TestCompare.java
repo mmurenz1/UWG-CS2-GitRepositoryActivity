4b9bb7d896510e4689b5c2ae532fd2cfacc68cce
Mickel Ishema Murenzi <murenzimickel1@gmail.com>
2025-10-24 13:56:32 -0400

    Task 2A - Business Logic for Sorting Bases on Name
    1) Updated Ascending.java toString() to return Ascending Priority for clarity
    2) Updated Descending,java toString() to return Descending Priority for clarity
    3) Created AscendingName.java comparator to sort tasks by name in ascending order
    4) Created DesceningName.java comparatpr to sort tasks by name in descending order
    5) Created TestCompare.java for AscendingName with tests for null checks and name comparisons
    6) Created TestCompare.java for DescendingName with tets for null checks and name comparisons

---
+++ code/Task Tracker/src/test/java/edu/westga/cs1302/task_tracker/model/descending_name/TestCompare.java
@@ -0,0 +1,85 @@
+package edu.westga.cs1302.task_tracker.model.descending_name;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.task_tracker.model.DescendingName;
+import edu.westga.cs1302.task_tracker.model.Task;
+import edu.westga.cs1302.task_tracker.model.Task.TaskPriority;
+
+class TestCompare {
+
+    @Test
+    void testO1IsNull() {
+        Task o1 = new Task("apple", "desc", TaskPriority.HIGH);
+        Task o2 = new Task("banana", "desc", TaskPriority.HIGH);
+        DescendingName descending = new DescendingName();
+        
+        assertThrows(IllegalArgumentException.class, ()->{descending.compare(null, o2);});
+    }
+    
+    @Test
+    void testO2IsNull() {
+        Task o1 = new Task("apple", "desc", TaskPriority.HIGH);
+        Task o2 = new Task("banana", "desc", TaskPriority.HIGH);
+        DescendingName descending = new DescendingName();
+        
+        assertThrows(IllegalArgumentException.class, ()->{descending.compare(o1, null);});
+    }
+    
+    @Test
+    void testO1AppleAndO2Banana() {
+        Task o1 = new Task("apple", "desc", TaskPriority.HIGH);
+        Task o2 = new Task("banana", "desc", TaskPriority.HIGH);
+        DescendingName descending = new DescendingName();
+        
+        int result = descending.compare(o1, o2);
+        
+        assertTrue(result > 0);
+    }
+    
+    @Test
+    void testO1BananaAndO2Apple() {
+        Task o1 = new Task("banana", "desc", TaskPriority.HIGH);
+        Task o2 = new Task("apple", "desc", TaskPriority.HIGH);
+        DescendingName descending = new DescendingName();
+        
+        int result = descending.compare(o1, o2);
+        
+        assertTrue(result < 0);
+    }
+    
+    @Test
+    void testO1AppleAndO2Apple() {
+        Task o1 = new Task("apple", "desc", TaskPriority.HIGH);
+        Task o2 = new Task("apple", "desc", TaskPriority.HIGH);
+        DescendingName descending = new DescendingName();
+        
+        int result = descending.compare(o1, o2);
+        
+        assertTrue(result == 0);
+    }
+    
+    @Test
+    void testO1ZebraAndO2Apple() {
+        Task o1 = new Task("zebra", "desc", TaskPriority.LOW);
+        Task o2 = new Task("apple", "desc", TaskPriority.HIGH);
+        DescendingName descending = new DescendingName();
+        
+        int result = descending.compare(o1, o2);
+        
+        assertTrue(result < 0);
+    }
+    
+    @Test
+    void testO1AppleAndO2Zebra() {
+        Task o1 = new Task("apple", "desc", TaskPriority.LOW);
+        Task o2 = new Task("zebra", "desc", TaskPriority.HIGH);
+        DescendingName descending = new DescendingName();
+        
+        int result = descending.compare(o1, o2);
+        
+        assertTrue(result > 0);
+    }
+}
